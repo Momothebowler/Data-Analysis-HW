87483d8a109cf61774112f6e132ad042ac63f4ea
Momothebowler <momothebowler@gmail.com>
2024-09-10 13:07:36 -0400

    Almost Done

--- Hw1.r
+++ Hw1.r
@@ -1,8 +1,29 @@
 data <- USArrests$UrbanPop
-plot(data)
-normalized <- dnorm(data, mean(data), sd(data))
-plot(data, normalized)
-cdf <- pnorm(data, mean(data), sd(data))
-plot(data,cdf)
-print(1-pnorm(60, mean(data), sd(data)))
-print(pnorm(50, mean(data), sd(data)) - (1 - pnorm(80, mean(data), sd(data))))
+# print(data)
+# plot(data)
+mu <- mean(data)
+std <- sd(data)
+normalized <- dnorm(data, mu, std)
+
+png(filename = "Normalized_Data.png")
+plot(data, normalized,
+    col = "blue",
+    xlab = "% of Peoples", ylab = "Density",
+    main = "Plot of densities for height data"
+)
+abline(v = mu, col = "red", lty = "dashed")
+dev.off()
+
+png(filename = "CDF.png")
+plot(data, pnorm(data, mean = mu, sd = std),
+    ylab = "Cumulative Distribution Function",
+    xlab = "% of People",
+    main = "CDF for height data"
+)
+abline(v = mu, col = "red", lty = "dashed")
+dev.off()
+
+print(pnorm(60, mean = mu, sd = std))
+print(pnorm(80, mean = mu, sd = std) - pnorm(50, mean = mu, sd = std))
+
+print(qnorm(0.75, mean = mu, sd = std))
