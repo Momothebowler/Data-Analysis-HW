bcfb84867be719d326d40fce760f52a1ac5ab62f
Momothebowler <momothebowler@gmail.com>
2024-11-15 16:46:07 -0500

    pictures

--- Hw 4/hw4Q2.r
+++ Hw 4/hw4Q2.r
@@ -2,10 +2,12 @@ library(alr4)
 data("UN11")
 
 # Fit a model with fertility as the response variable and ppgdp, lifeExpF, and pctUrban as predictor
-
 un11_sub <- UN11[, c("fertility", "ppgdp", "lifeExpF", "pctUrban")]
 # fit the regression model
 mod <- lm(fertility ~ ppgdp + lifeExpF + pctUrban, data = UN11)
+png(filename = "residuals.png")
+plot(mod)
+dev.off()
 summary(mod)
 
 X <- model.matrix(mod)
@@ -13,7 +15,9 @@ rownames(X) <- NULL # remove row (car) names
 attr(X, "assign") <- NULL # remove attributes
 attr(X, "contrasts") <- NULL # remove attributes
 
+png(filename = "scatterofpairs.png")
 pairs(un11_sub)
+dev.off()
 
 # log trends, I do not believe linear will work here
 
@@ -27,8 +31,12 @@ un11_sub$ppgdp <- log(UN11$ppgdp)
 # un11_sub
 mod2 <- lm(fertility ~ ., data = un11_sub)
 summary(mod2)
+png(filename = "logpp.png")
 pairs(un11_sub)
+dev.off()
 
 un11_sub$fertility <- log(UN11$fertility)
+png(filename = "multilog.png")
 pairs(un11_sub)
+dev.off()
 # I believe now a linear model is appropriate
